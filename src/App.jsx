5dd55a96edd97b7ab8e544da1273fa3f0fa1dcf5
smichaelonline <stephaniemichael001@gmail.com>
2022-11-07 19:23:29 -0500

    set up nevbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -8,13 +9,16 @@ import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<Contact />} />
-      <Route path='resume' element={<Resume />} />
-      <Route path='projects' element={<Projects />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='resume' element={<Resume />} />
+        <Route path='projects' element={<Projects />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav>
+      <Link to='/'>
+        Stephanie Michael 
+      </Link>
+      <ul>
+        <li>
+          <Link to='/about'>
+            About
+          </Link>
+        </li>
+
+        <li>
+          <Link to='/contact'>
+            Contact
+          </Link>
+        </li>
+
+        <li>
+          <Link to='/resume'>
+            Resume
+          </Link>
+        </li>
+
+        <li>
+          <Link to='/projects'>
+            Projects
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
