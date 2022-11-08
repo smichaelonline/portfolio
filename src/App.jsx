56ce600067643a098b18e1176a29f91f36d786fc
smichaelonline <stephaniemichael001@gmail.com>
2022-11-07 19:16:46 -0500

    route all pages

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Resume from './pages/Resume'
+import Projects from './pages/Projects'
+import './App.css';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='resume' element={<Resume />} />
+      <Route path='projects' element={<Projects />} />
+    </Routes>
+  )
+}
+
+export default App;
