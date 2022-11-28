7beeafa70cc80c6ccbbb0a1d4b998881f62810bb
smichaelonline <stephaniemichael001@gmail.com>
2022-11-27 22:43:39 -0500

    update contact section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,10 +7,8 @@ function About() {
       <img src={headshot} alt="Headshot of Stephanie Michael" className={styles.headshotImg}/>
       <div className={styles.bioDiv}>
         <h1 className={styles.intro}>I'm Stephanie. Nice to meet you.</h1>
-        <div className={styles.bio}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu mi bibendum neque egestas congue quisque egestas diam. Proin libero nunc consequat interdum varius sit amet. Sit amet facilisis magna etiam. Bibendum arcu vitae elementum curabitur vitae. Lacus sed turpis tincidunt id aliquet risus feugiat. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Suspendisse in est ante in nibh. Donec et odio pellentesque diam volutpat. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. In nisl nisi scelerisque eu ultrices.</p>
-
-          <p>Amet nulla facilisi morbi tempus iaculis urna id. Vitae ultricies leo integer malesuada. Orci porta non pulvinar neque laoreet suspendisse interdum. Et ligula ullamcorper malesuada proin libero nunc. Sit amet facilisis magna etiam tempor. Feugiat sed lectus vestibulum mattis. Dictum varius duis at consectetur lorem donec massa sapien. Consequat id porta nibh venenatis. Sollicitudin aliquam ultrices sagittis orci a scelerisque. Erat velit scelerisque in dictum non. Urna id volutpat lacus laoreet non curabitur gravida arcu. Ac turpis egestas sed tempus urna et pharetra pharetra. Sed blandit libero volutpat sed cras ornare arcu. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Amet justo donec enim diam. Arcu dui vivamus arcu felis bibendum. Vel fringilla est ullamcorper eget. Mattis enim ut tellus elementum sagittis vitae et leo duis. Convallis aenean et tortor at. Imperdiet massa tincidunt nunc pulvinar sapien.</p>
+        <div className={styles.bio} >
+          <p>As a public relations professional turned Software Engineer, I am passionate about brand development and alignment which creates an ideal synergy with software engineering. My ability to consistently be prepared for any situation allows me to look at the bigger picture and make large problems manageable. These skill sets prepare me for building new, eye-catching products for consumers while simultaneously keeping in mind the human experience.</p>
         </div>
       </div>
     </div>
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,25 @@
 import styles from './Contact.module.css'
 
+import LinkedIn from '../../assets/icons/linkedin.svg'
+import Github from '../../assets/icons/github.svg'
+import Mail from '../../assets/icons/mail.svg'
+
 function Contact() {
   return (
     <div id="contact" className={styles.contactDiv}>
       <h1 className={styles.contact}>Contact</h1>
       <div className={styles.contactInfo}>
-        <h3>Reach out if you'd like to work with me!</h3>
-        <p>LinkedIn</p>
-        <p>Github</p>
-        <p>Email</p>
+        <h3>I'd love to hear from you!</h3>
+        <a href="https://www.linkedin.com/in/stephaniemichael001/">
+          <img src={LinkedIn} className={styles.logoIcons}alt="LinkedIn Logo" />
+        </a>
+        <p className={styles.handleLI}>@stephaniemichael001</p>
+        <a href="https://github.com/smichaelonline">
+          <img src={Github} alt="Github Logo" className={styles.logoIcons} />
+        </a>
+        <p className={styles.handleGH}>@smichaelonline</p>
+        <img src={Mail} alt="Gmail Logo" className={styles.logoIcons} />
+        <p className={styles.handleEmail}>stephaniemichael001@gmail.com</p>
       </div>
     </div>
   )
